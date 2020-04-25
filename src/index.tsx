46a06a2285f1dc5e0eadd582e62589aa572542bc
oguzhan.kahyaoglu <oguzhan.kahyaoglu@enerjisauretim.com>
2020-04-25 14:54:03 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,116 @@
+import * as React from 'react';
+import {render} from 'react-dom';
+import {Spinner} from '@contentful/forma-36-react-components';
+import {init, FieldExtensionSDK, EntrySys, Link} from 'contentful-ui-extensions-sdk';
+import '@contentful/forma-36-react-components/dist/styles.css';
+import './index.css';
+
+interface AppProps {
+    sdk: FieldExtensionSDK;
+}
+
+interface AppState {
+    value: Link;
+    error: boolean,
+    hasLoaded: boolean,
+    items: any[]
+}
+
+interface EntryItem{
+    sys: EntrySys,
+    fields: any
+}
+
+export class App extends React.Component<AppProps, AppState> {
+    constructor(props: AppProps) {
+        super(props);
+        this.state = {
+            value: props.sdk.field.getValue(),
+            error: false,
+            hasLoaded: false,
+            items: []
+        };
+    }
+
+    toFieldValue(id: string): Link {
+        return {
+            sys: {
+                id: id,
+                linkType: 'Entry',
+                type: 'Link'
+            }
+        };
+    }
+
+    detachExternalChangeHandler: Function | null = null;
+    selectedItem = "";
+
+    componentDidMount() {
+        this.props.sdk.window.startAutoResizer();
+
+        // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
+        this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(this.onExternalChange);
+        console.log('field:', this.props.sdk.field);
+        console.log('value:', this.props.sdk.field.getValue());
+        this.props.sdk.space.getPublishedEntries<EntryItem>({content_type: 'category', limit: 1000})
+            .then(resp => {
+                let items = resp.items.map(r => {
+                    let title = r.fields['title'][Object.keys(r.fields['title'])[0]];
+                    return {value: r.sys.id, display: title};
+                });
+                this.setState({hasLoaded: true, items: items});
+                console.log('state loaded:', this.state);
+                this.selectedItem = this.state.value.sys.id;
+            });
+    }
+
+    componentWillUnmount() {
+        if (this.detachExternalChangeHandler) {
+            this.detachExternalChangeHandler();
+        }
+    }
+
+    onExternalChange = (value: any) => {
+        this.setState({value});
+    };
+
+    onChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = e.currentTarget.value;
+        console.log('new value:', value, 'selectedItem', this.selectedItem, 'state', this.state);
+        this.selectedItem = value;
+        if (value) {
+            let newFieldValue = this.toFieldValue(value);
+            await this.props.sdk.field.setValue(newFieldValue);
+        } else {
+            await this.props.sdk.field.removeValue();
+        }
+    };
+
+    render = () => {
+        if (!this.state.hasLoaded) {
+            return (<Spinner/>);
+        }
+        return (
+            <div>
+                <select
+                    value={this.selectedItem}
+                    onChange={this.onChange}
+                >
+                    {this.state.items.map((item) => <option key={item.value} value={item.value}>{item.display}</option>)}
+                </select>
+            </div>
+        );
+    };
+}
+
+init(sdk => {
+    render(<App sdk={sdk as FieldExtensionSDK}/>, document.getElementById('root'));
+});
+
+/**
+ * By default, iframe of the extension is fully reloaded on every save of a source file.
+ * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
+ */
+if (module.hot) {
+    module.hot.accept();
+}
